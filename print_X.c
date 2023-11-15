df290d13b23e8b1d58b232224ac890e775d81de2
Tunde <tundeogunmodede1@gmail.com>
2023-11-15 10:43:24 -0800

    printf

--- print_X.c
+++ print_X.c
@@ -8,7 +8,9 @@ int print_X(va_list args)
 {
 	int digit = 0;
 	int rem;
+	int i;
 	unsigned int num = va_arg(args, unsigned int);
+	char buffer[20];
 
 	if (num == 0)
 	{
@@ -20,12 +22,18 @@ int print_X(va_list args)
 		rem = num % 16;
 
 		if (rem < 10)
-			_putchar(rem + '0');
+			buffer[digit++] = rem + '0';
 		else
-			_putchar(rem + 'A' - 10);
+			buffer[digit++] = rem + 'A' - 10;
 
 		num = num / 16;
-		digit++;
 	}
-	return (digit);
+
+	for (i = 0; i < 8 - digit; ++i)
+		_putchar('0');
+
+	for (i = digit - 1; i >= 0; --i)
+		_putchar(buffer[i]);
+
+	return (8);
 }
